d71e8904d6edee4a59d3be145f454c58dc67aa30
Anatoly T <antt001@gmail.com>
2018-05-13 17:00:36 +0300

    fix: cange location format

--- src/station-id-map.js
+++ src/station-id-map.js
@@ -2,43 +2,55 @@ export default {
     1: {
         id: '0x4fFCe6A06CB4317a295acaDdbb3141c73158f954',
         name: 'Rosie',
-        longitude: 116.419352, 
-        latitude: 38.802660,
+        location: {
+            longitude: 116.419352,
+            latitude: 38.802660
+        },
         status: 'Available',
     },
     2: {
         id: '0xC5DB1c548eCF910beFF0524A44B2c94ff2A7f3Fe',
         name: 'Bender',
-        longitude: 116.419352, 
-        latitude: 38.802660,
+        location: {
+            longitude: 116.419352, 
+            latitude: 38.802660,
+        },
         status: 'Awaiting Landing Approval',
     },
     3: {
         id: '0xE835BAF20DcB333F8BF8D6dA90Dc4f3c0A9FFC34',
         name: 'Nairobi',
-        longitude: 116.419352, 
-        latitude: 38.802660,
+        location: {
+            longitude: 116.419352, 
+            latitude: 38.802660,
+        },
         status: 'Charging',
     },
     4: {
         id: '0x9103C63f2BAB52cde4CA825f9C0Ab73eB8E49110',
         name: 'Rusty',
-        longitude: 116.419352, 
-        latitude: 38.802660,
+        location: {
+            longitude: 116.419352, 
+            latitude: 38.802660,
+        },
         status: 'Available',
     },
     5: {
         id: '0x5baEAD6AC29DdAF780e7B3e35b83DeFE9159F44B',
         name: 'Isumoid',
-        longitude: 116.419352, 
-        latitude: 38.802660,
+        location: {
+            longitude: 116.419352, 
+            latitude: 38.802660,
+        },
         status: 'Charging',
     },
     6: {
         id: '0x1020f3df4C039a860525E3C8a2F844518B39A1EB',
         name: 'Golem',
-        longitude: 116.419352, 
-        latitude: 38.802660,
+        location: {
+            longitude: 116.419352, 
+            latitude: 38.802660,
+        },
         status: 'Awaiting Landing Approval',
     },
 };
