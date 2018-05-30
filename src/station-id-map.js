25dc224fca8ad0a8688ca3d8b03cf8e5ddfcb50c
yaronsha <yaron.shapira7@gmail.com>
2018-05-30 09:52:29 +0300

    change dav id to main net address

--- src/station-id-map.js
+++ src/station-id-map.js
@@ -1,7 +1,7 @@
 const [latitude, longitude] = process.env.SKY_LOCATION.split(',').map(v => parseFloat(v));
 module.exports = {
     11: {
-        davId: '0x1df62f291b2e969fb0849d99d9ce41e2f137006e',
+        davId: '0x59a469331ebc94e97b579e66de2f92578379777f',
         id: 11,
         name: 'Rosie',
         price: '22e16',
@@ -14,7 +14,7 @@ module.exports = {
         drone_types: ['mavic', 'matrice', 'phantom']
     },
     12: {
-        davId: '0xaca94ef8bd5ffee41947b4585a84bda5a3d3da6e',
+        davId: '0xd319031c21f05b576ce662057190e8c49fdb5836',
         id: 12,
         name: 'Bender',
         price: '5e16',
@@ -27,7 +27,7 @@ module.exports = {
         drone_types: ['mavic', 'matrice', 'phantom']
     },
     13: {
-        davId: '0xF0D5BC18421fa04D0a2A2ef540ba5A9f04014BE3',
+        davId: '0x397776b35874f8c84ce288fedd9b684118774386',
         id: 13,
         name: 'Nairobi',
         price: '4e16',
@@ -40,7 +40,7 @@ module.exports = {
         drone_types: ['mavic']
     },
     14: {
-        davId: '0x66FC63C2572bF3ADD0Fe5d44b97c2E614E35e9a3',
+        davId: '0x680a0047b7b1af651d920722ea70cc0c055f4693',
         id: 14,
         name: 'Rusty',
         price: '5e16',
@@ -53,7 +53,7 @@ module.exports = {
         drone_types: ['matrice', 'phantom']
     },
     15: {
-        davId: '0xf408f04F9b7691f7174FA2bb73ad6d45fD5d3CBe',
+        davId: '0xe71275245102f4b85092765aba7d29efdcb079cf',
         id: 15,
         name: 'Isumoid',
         price: '6e16',
@@ -65,17 +65,17 @@ module.exports = {
         status: 'Available',
         drone_types: ['mavic', 'phantom']
     },
-    16: {
-        davId: '0x2F560290FEF1B3Ada194b6aA9c40aa71f8e95598',
-        id: 16,
-        name: 'Golem',
-        price: '7e16',
-        location: {
-            longitude: longitude,
-            latitude: latitude
-        },
-        max_charging_velocity: 30,
-        status: 'Available',
-        drone_types: ['phantom']
-    },
+    // 16: {
+    //     davId: '0x2F560290FEF1B3Ada194b6aA9c40aa71f8e95598',
+    //     id: 16,
+    //     name: 'Golem',
+    //     price: '7e16',
+    //     location: {
+    //         longitude: longitude,
+    //         latitude: latitude
+    //     },
+    //     max_charging_velocity: 30,
+    //     status: 'Available',
+    //     drone_types: ['phantom']
+    // },
 };
